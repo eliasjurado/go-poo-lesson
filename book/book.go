7cb559d588791aff693926a29e72dcc85b30e45a
Elias Jurado <ridimdev@gmail.com>
2024-03-19 18:53:51 -0500

    encapsulamiento - setters y getters

--- book/book.go
+++ book/book.go
@@ -19,3 +19,30 @@ func NewBook(title string, author string, pages int) *Book {
 func (b *Book) PrintInfo() {
 	fmt.Printf("Title: %s \nAuthor: %s\nPages: %d\n", b.Title, b.Author, b.Pages)
 }
+
+func (book *Book) GetTitle() string {
+	return book.Title
+}
+
+func (book *Book) GetAuthor() string {
+	return book.Author
+}
+
+func (book *Book) GetPages() int {
+	return book.Pages
+}
+
+func (book *Book) SetTitle(Title string) *Book {
+	book.Title = Title
+	return book
+}
+
+func (book *Book) SetAuthor(Author string) *Book {
+	book.Author = Author
+	return book
+}
+
+func (book *Book) SetPages(Pages int) *Book {
+	book.Pages = Pages
+	return book
+}
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
-import "library/book"
+import (
+	"fmt"
+	"library/book"
+)
 
 func main() {
 	var myBook = book.Book{
-		Title: "Moby Dick",
+		Title:  "Moby Dick",
 		Author: "Herman Van Dicke",
-		Pages: 543,
+		Pages:  543,
 	}
 	myBook.PrintInfo()
+
+	myBook.SetTitle("Moby Dick (Edición Especial)")
+	fmt.Println(myBook.GetTitle())
 
 	mySecondBook := book.NewBook("La Ciudad Y Los Perros", "Mario Vargas Llosa", 843)
 	mySecondBook.PrintInfo()
